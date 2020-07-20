ced00cc51814b2735e40e5ed9f7bd4a797f19d15
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-07-20 15:05:27 +0800

    show auto logout timer

--- js/login.js
+++ js/login.js
@@ -21,6 +21,7 @@ async function logout(eventCode = 0) {
       function () {
         if (eventCode === 0) {
           alert("登出成功");
+          clearInterval(oCountdownID);
         } else if (eventCode === 1) {
           alert("您已閒置超過7分鐘，系統自動登出");
         } else {
@@ -64,6 +65,7 @@ firebase.auth().onAuthStateChanged(async function (user) {
     );
     if (userIsAdmin === true) {
       console.log("logging");
+      ReCalculate();
       var currentdate = new Date();
       var datetime =
         currentdate.getFullYear() +
@@ -87,6 +89,7 @@ firebase.auth().onAuthStateChanged(async function (user) {
       var divContent = `
       <div class="admin__content__title">歡迎管理員 ${userName}大大</div>
       <div class="admin__content__title" style="font-weight: 300; font-size: 16px;">上次上線於 ${leastOnlineTime}</div>
+      <div class="admin__content__title" id="autoLogoutCountDown" style="font-weight: 300; font-size: 16px;">系列於 後自動登出</div>
       <div class="admin__content__logout" onclick="logout()">
           <div class="admin__content__logout__btn">登出系統</div>
       </div>
@@ -133,23 +136,38 @@ firebase.auth().onAuthStateChanged(async function (user) {
   $("#admin__content").append(divContent);
 });
 console.log(user);
-var oTimerId;
+console.log(tempUser);
+var oCountdownID;
+var countdownClock = 0;
 function Timeout() {
   console.log("autoLogout");
   logout(1);
 }
 function ReCalculate() {
-  clearTimeout(oTimerId);
-  if (user != "") {
-    oTimerId = setTimeout(function () {
-      Timeout(1);
-    }, 7 * 60 * 1000);
+  var timer = 7 * 60;
+  clearInterval(oCountdownID);
+  if (user != "" || tempUser != "") {
+    console.log("start count");
+    oCountdownID = setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      if (--timer < 0) {
+        Timeout(1);
+        clearInterval(oCountdownID);
+      }
+      document.getElementById("autoLogoutCountDown").innerHTML =
+        "系統於 " + minutes + ":" + seconds + " 後自動登出";
+      //console.log(minutes + ":" + seconds);
+    }, 1000);
   }
 }
+console.log(countdownClock);
 document.onmousedown = ReCalculate;
 document.onmousemove = ReCalculate;
 document.onkeydown = ReCalculate;
-ReCalculate();
+//ReCalculate();
 
 function getUserName() {
   return user;

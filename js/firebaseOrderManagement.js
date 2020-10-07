101097efe69307a540e34dd6fa2e9e5b2bd336bb
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-10-07 19:36:16 +0800

    update twd to hkd = 3.6

--- js/firebaseOrderManagement.js
+++ js/firebaseOrderManagement.js
@@ -13,7 +13,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 var db = firebase.firestore();
-var twdToHKD = 3.7;
+var twdToHKD = 3.6;
 //show order item list
 function showOrderItem(mode) {
   console.log(mode);
